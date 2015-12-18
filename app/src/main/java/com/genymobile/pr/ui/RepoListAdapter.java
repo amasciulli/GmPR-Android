139f42384174299054ffff36064eb64d896cd75c
Alexandre Masciulli <amasciulli@genymobile.com>
2015-12-18 16:12:58 +0100

    Use specific layout for repo item

--- app/src/main/java/com/genymobile/pr/ui/RepoListAdapter.java
+++ app/src/main/java/com/genymobile/pr/ui/RepoListAdapter.java
@@ -14,33 +14,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RepoListAdapter extends RecyclerView.Adapter<RepoListAdapter.ViewHolder> {
+    private static final int TYPE_REPO = 0;
+    private static final int TYPE_PULL_REQUEST = 1;
+
     private List<Object> list = new ArrayList<>();
     private ItemClickListener<PullRequest> itemClickListener;
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View root = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_repo, parent, false);
-        return new ViewHolder(root);
+
+        View root;
+        switch (viewType) {
+            case TYPE_REPO:
+                root = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_repo, parent, false);
+                return new RepoViewHolder(root);
+            case TYPE_PULL_REQUEST:
+                root = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_pull_request, parent, false);
+                return new PullRequestViewHolder(root);
+            default:
+                throw new IllegalArgumentException("Unknown type");
+        }
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
-        final Object item = list.get(position);
-        if (item instanceof Repo) {
-            holder.nameView.setText(((Repo) item).getName());
-        } else {
-            holder.nameView.setText(((PullRequest) item).getTitle());
-            if (itemClickListener != null) {
-                holder.itemView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        itemClickListener.onClick((PullRequest) item, position);
-                    }
-                });
-            }
+        switch (getItemViewType(position)) {
+            case TYPE_REPO:
+                bindRepoViewHolder((RepoViewHolder) holder, (Repo) list.get(position));
+                break;
+            case TYPE_PULL_REQUEST:
+                bindPullRequestViewHolder((PullRequestViewHolder) holder, (PullRequest) list.get(position));
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown type");
         }
+    }
 
+    private void bindRepoViewHolder(RepoViewHolder holder, Repo repo) {
+        holder.nameView.setText(repo.getName());
+    }
+
+    private void bindPullRequestViewHolder(PullRequestViewHolder holder, PullRequest pullRequest) {
+        holder.titleView.setText(pullRequest.getTitle());
     }
 
     @Override
@@ -59,12 +76,32 @@ public class RepoListAdapter extends RecyclerView.Adapter<RepoListAdapter.ViewHo
         notifyItemRangeInserted(previousCount - 1, pullRequests.size() + 1);
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView nameView;
+    @Override
+    public int getItemViewType(int position) {
+        return list.get(position) instanceof Repo ? TYPE_REPO : TYPE_PULL_REQUEST;
+    }
 
+    abstract class ViewHolder extends RecyclerView.ViewHolder {
         public ViewHolder(View itemView) {
             super(itemView);
+        }
+    }
+
+    class RepoViewHolder extends ViewHolder {
+        private final TextView nameView;
+
+        public RepoViewHolder(View itemView) {
+            super(itemView);
             nameView = (TextView) itemView.findViewById(R.id.name);
+        }
+    }
+
+    class PullRequestViewHolder extends ViewHolder {
+        private final TextView titleView;
+
+        public PullRequestViewHolder(View itemView) {
+            super(itemView);
+            titleView = (TextView) itemView.findViewById(R.id.title);
         }
     }
 }
