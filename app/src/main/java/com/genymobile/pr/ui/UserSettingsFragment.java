f950d4d04ea3d23e082ec4556008f41a67282296
Alexandre Masciulli <amasciulli@genymobile.com>
2015-12-20 22:26:30 +0100

    Begin user settings fragment layout

---
+++ app/src/main/java/com/genymobile/pr/ui/UserSettingsFragment.java
@@ -0,0 +1,18 @@
+package com.genymobile.pr.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.genymobile.pr.R;
+
+public class UserSettingsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return LayoutInflater.from(getActivity()).inflate(R.layout.fragment_user_settings, container, false);
+    }
+}
