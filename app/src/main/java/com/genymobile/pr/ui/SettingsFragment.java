15b1db5e8031c46701d0085cb4e6200c2a1a4232
Alexandre Masciulli <amasciulli@genymobile.com>
2015-12-21 15:06:18 +0100

    Add settings activity to edit login and password

---
+++ app/src/main/java/com/genymobile/pr/ui/SettingsFragment.java
@@ -0,0 +1,15 @@
+package com.genymobile.pr.ui;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.genymobile.pr.R;
+
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+}
