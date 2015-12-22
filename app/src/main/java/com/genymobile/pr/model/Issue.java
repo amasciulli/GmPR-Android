7b8d005298847a8cb6979691ab395a54d489ba1f
pmarois <pmarois@genymobile.com>
2015-12-22 10:49:57 +0100

    Add Issue and Label Model Objects

---
+++ app/src/main/java/com/genymobile/pr/model/Issue.java
@@ -0,0 +1,12 @@
+package com.genymobile.pr.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Issue {
+    private List<Label> labels = new ArrayList<>();
+
+    public List<Label> getLabels() {
+        return labels;
+    }
+}
---
+++ app/src/main/java/com/genymobile/pr/model/Label.java
@@ -0,0 +1,20 @@
+package com.genymobile.pr.model;
+
+public class Label {
+    private String url;
+    private String name;
+    private String color;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
