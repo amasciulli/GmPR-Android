2d8bde137fd864162e1fdf4876115b0af6ccf62b
pmarois <pmarois@genymobile.com>
2015-12-28 18:02:36 +0100

    Set the title in the dialog
    + Some refactoring

--- app/src/main/java/com/genymobile/pr/ui/PullRequestDetailsDialogFragment.java
+++ app/src/main/java/com/genymobile/pr/ui/PullRequestDetailsDialogFragment.java
@@ -8,14 +8,17 @@ import com.genymobile.pr.model.Label;
 import com.genymobile.pr.net.GitHubProvider;
 import com.genymobile.pr.net.IssueCallback;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.view.LayoutInflater;
+import android.support.v7.app.AlertDialog;
+import android.util.DisplayMetrics;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
@@ -27,7 +30,7 @@ public class PullRequestDetailsDialogFragment extends DialogFragment {
     private static final String ARG_TITLE = "title";
     private static final String ARG_NUMBER = "number";
     private static final String ARG_REPO = "repo";
-    private static final int DEFAULT_SPACING = 10;
+    private static final int DEFAULT_SPACING_PX = 5;
     private int number;
     private String repo;
     private String organization;
@@ -57,6 +60,18 @@ public class PullRequestDetailsDialogFragment extends DialogFragment {
         getIssue();
     }
 
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Custom Layouts are created with a null parent as per https://developer.android.com/guide/topics/ui/dialogs.html section Creating a Custom Layout
+        @SuppressLint("InflateParams")
+        View root = getActivity().getLayoutInflater().inflate(R.layout.pull_request_details_fragment_dialog, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        return builder.setTitle(title).setView(root).create();
+    }
+
     private void getIssue() {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String login = preferences.getString(getString(R.string.pref_login), null);
@@ -68,27 +83,28 @@ public class PullRequestDetailsDialogFragment extends DialogFragment {
     @Subscribe
     @SuppressWarnings("unused")
     public void onIssueRetrieved(IssueRetrievedEvent event) {
+        int defaultSpacing = dpToPx(DEFAULT_SPACING_PX);
         LinearLayout labelsView = (LinearLayout) PullRequestDetailsDialogFragment.this.getDialog().findViewById(R.id.labels);
         LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(DEFAULT_SPACING, DEFAULT_SPACING, DEFAULT_SPACING, DEFAULT_SPACING);
+        layoutParams.setMargins(defaultSpacing, defaultSpacing, defaultSpacing, defaultSpacing);
         Issue issue = event.getIssue();
         for (Label label : issue.getLabels()) {
             addLabel(labelsView, label, layoutParams);
         }
     }
 
+    public int dpToPx(int dp) {
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
     private void addLabel(LinearLayout labelsView, Label label, LayoutParams layoutParams) {
         TextView textView = new TextView(getContext());
         textView.setLayoutParams(layoutParams);
         textView.setText(label.getName());
-        textView.setPadding(DEFAULT_SPACING, DEFAULT_SPACING, DEFAULT_SPACING, DEFAULT_SPACING);
+        textView.setPadding(DEFAULT_SPACING_PX, DEFAULT_SPACING_PX, DEFAULT_SPACING_PX, DEFAULT_SPACING_PX);
         textView.setBackgroundColor(Color.parseColor("#" + label.getColor()));
         labelsView.addView(textView);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pull_request_details_fragment_dialog, container, false);
     }
 
     @Override
